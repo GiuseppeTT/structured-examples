a481f09f3887f8d2fee013f95b9276af6e0f7a9a
GiuseppeTT <giu.tinti@gmail.com>
2021-07-02 15:56:34 -0300

    Fill basic-script project

--- basic-script/analysis.R
+++ basic-script/analysis.R
@@ -1,5 +1,6 @@
 # Load packages ----------------------------------------------------------------
 library(tidyverse)
+library(gt)
 
 
 # Source auxiliary R files -----------------------------------------------------
@@ -7,4 +8,34 @@ source("R/functions.R")
 
 
 # Analysis ---------------------------------------------------------------------
-print(1 + 1)
+data <- read_csv("data/diamonds.csv")
+
+table <-
+    data %>%
+    summarise(
+        Minimum = min(price),
+        Mean = mean(price),
+        Maximum = max(price),
+        `Standard Deviation` = sd(price)
+    ) %>%
+    pivot_longer(everything(), names_to = "Metric", values_to = "Value") %>%
+    gt() %>%
+    fmt_number(Value, decimals = 0, pattern = "${x}")
+
+gtsave(table, "output/table.html")
+
+plot <-
+    data %>%
+    ggplot(aes(x = carat, y = price)) +
+    geom_point(alpha = 0.1) +
+    scale_x_log10() +
+    scale_y_log10()
+
+ggsave("output/plot.png", plot)
+
+model <-
+    data %>%
+    lm(log(price) ~ log(carat), data = .)  %>%
+    summary()
+
+write_output(model, "output/model.txt")
