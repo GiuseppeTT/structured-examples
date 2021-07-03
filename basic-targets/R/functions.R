446785eabe30ef9593a7e8dce9951047209af543
GiuseppeTT <giu.tinti@gmail.com>
2021-07-03 00:22:14 -0300

    Fill basic-targets project

--- basic-targets/R/functions.R
+++ basic-targets/R/functions.R
@@ -1,1 +1,39 @@
 # Define auxiliary functions ---------------------------------------------------
+read_data <- function(
+    path
+) {
+    path %>% 
+        read_csv()
+}
+
+table_metrics <- function(
+    data
+) {
+    data %>%
+        summarise(
+            Minimum = min(price),
+            Mean = mean(price),
+            Maximum = max(price),
+            `Standard Deviation` = sd(price)
+        ) %>%
+        pivot_longer(everything(), names_to = "Metric", values_to = "Value") %>%
+        gt() %>%
+        fmt_number(Value, decimals = 0, pattern = "${x}")
+}
+
+plot_relationship <- function(
+    data
+) {
+    data %>%
+        ggplot(aes(x = carat, y = price)) +
+        geom_point(alpha = 0.1) +
+        scale_x_log10() +
+        scale_y_log10()
+}
+
+fit_model <- function(
+    data
+) {
+    data %>%
+        lm(log(price) ~ log(carat), data = .)
+}
--- basic-targets/_targets.R
+++ basic-targets/_targets.R
@@ -1,5 +1,6 @@
 # Load packages ----------------------------------------------------------------
 library(tidyverse)
+library(gt)
 library(targets)
 library(tarchetypes)
 
@@ -10,8 +11,24 @@ source("R/functions.R")
 
 # Define analysis targets ------------------------------------------------------
 list(
-  tar_target(
-      complex_algebra,
-      1 + 1
-  )
+    tar_file(
+        data_file,
+        "data/diamonds.csv"
+    ),
+    tar_target(
+        data,
+        read_data(data_file)
+    ),
+    tar_target(
+        table,
+        table_metrics(data)
+    ),
+    tar_target(
+        plot,
+        plot_relationship(data)
+    ),
+    tar_target(
+        model,
+        fit_model(data)
+    )
 )
