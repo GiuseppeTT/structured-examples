5ecee27849821dcc05e9ef58e0581537b1101097
GiuseppeTT <giu.tinti@gmail.com>
2021-07-03 00:34:35 -0300

    Add and fill extension-subfolder-basic-script project

---
+++ extension-subfolder-basic-script/analysis.R
@@ -0,0 +1,41 @@
+# Load packages ----------------------------------------------------------------
+library(tidyverse)
+library(gt)
+
+
+# Source auxiliary R files -----------------------------------------------------
+source("R/functions.R")
+
+
+# Analysis ---------------------------------------------------------------------
+data <- read_csv("data/clean/diamonds.csv")
+
+table <-
+    data %>%
+    summarise(
+        Minimum = min(price),
+        Mean = mean(price),
+        Maximum = max(price),
+        `Standard Deviation` = sd(price)
+    ) %>%
+    pivot_longer(everything(), names_to = "Metric", values_to = "Value") %>%
+    gt() %>%
+    fmt_number(Value, decimals = 0, pattern = "${x}")
+
+gtsave(table, "output/table/table.html")
+
+plot <-
+    data %>%
+    ggplot(aes(x = carat, y = price)) +
+    geom_point(alpha = 0.1) +
+    scale_x_log10() +
+    scale_y_log10()
+
+ggsave("output/figure/plot.png", plot)
+
+model <-
+    data %>%
+    lm(log(price) ~ log(carat), data = .)  %>%
+    summary()
+
+write_output(model, "output/model/model.txt")
